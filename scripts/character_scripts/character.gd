5ff6a0a831aa5b949e79fa6a2ae1cd318cc25076
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2024-11-02 12:58:38 -0400

    start cleaning up scripts and scenes

--- scripts/character_scripts/character.gd
+++ scripts/character_scripts/character.gd
@@ -4,17 +4,44 @@ class_name Character
 
 @export var character_name:String
 @export var texture:Texture2D
-@export var isPlayer:bool
-var cam:Camera3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	cam = get_viewport().get_camera_3d()
 	look_at(Vector3(cam.global_position.x, global_position.y, cam.global_position.z),Vector3.UP,true)
-	if isPlayer:
-		cam.top_level = false #to make cam move with player
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if !isPlayer:
-		look_at(Vector3(cam.global_position.x, global_position.y, cam.global_position.z),Vector3.UP,true)
+
+
+func _physics_process(delta):
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
