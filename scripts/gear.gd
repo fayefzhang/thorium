4511ecd29a8c1f0e0659433795490158eeb88126
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2024-11-02 20:46:36 -0400

    make working gears

---
+++ scripts/gear.gd
@@ -0,0 +1,27 @@
+extends StaticBody3D
+
+var rotationTween: Tween = null
+var timeForFullRotation: float = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func start(direction: int) -> void:
+	rotationTween = create_tween()
+	rotationTween.tween_property(self, "rotation_degrees:z", direction * 360, timeForFullRotation)
+	rotationTween.tween_property(self, "rotation_degrees:z", 0, 0)
+	rotationTween.set_loops()
+
+func startWithRotationTime(time: float, direction: int) -> void:
+	timeForFullRotation = time
+	start(direction)
+
+func stop() -> void:
+	rotationTween.kill()
