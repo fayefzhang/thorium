727add9b6f264035a0e80b35bac882ed041b0b74
exaCORE42 <40a15b8f-bd19-4c61-844c-45c0c4aab94f@aleeas.com>
2024-11-03 10:00:43 -0500

    remove test printing

--- scripts/goop.gd
+++ scripts/goop.gd
@@ -11,7 +11,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(position.y)
 	if isRising:
 		position.y += riseRate * delta
 
